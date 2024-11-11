a58e81040553f5fe6d76393b059bb72989d48660
lise-charlotte <lisecharlottekoder@gmail.com>
2024-11-11 10:25:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# undervisning13
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/lise-charlotte/undervisning13)
---
+++ script.js
@@ -0,0 +1,7 @@
+const userName = 'Lise';
+const userAge = 25;
+let isOnline = true;
+
+isOnline = false;
+
+console.log(userName, userAge, isOnline);
